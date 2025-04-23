df31d31f1515b257ffe83b8eb405cbd0132f981a
shanglanxin <shanglanxin@gmail.com>
2025-04-23 22:46:16 +0900

    mapview add shop info.

--- app/src/main/java/com/kouusei/restaurant/presentation/mapview/MapView.kt
+++ app/src/main/java/com/kouusei/restaurant/presentation/mapview/MapView.kt
@@ -1,9 +1,15 @@
 package com.kouusei.restaurant.presentation.mapview
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
@@ -14,16 +20,18 @@ import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.kouusei.restaurant.presentation.RestaurantViewState
+import com.kouusei.restaurant.presentation.listview.RestaurantItemBar
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
 fun MapView(
-    viewState: RestaurantViewState.Success
+    viewState: RestaurantViewState.Success,
+    onNavDetail: (id: String) -> Unit
 ) {
     val center = viewState.boundingBox.center
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(center, 10f)
+        position = CameraPosition.fromLatLngZoom(center, 30f)
     }
     val scope = rememberCoroutineScope()
 
@@ -31,22 +39,42 @@ fun MapView(
         zoomAll(scope, cameraPositionState, viewState.boundingBox)
     }
 
-    GoogleMap(
-        modifier = Modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
+    var selectedShop = remember { mutableStateOf(viewState.shopList.first()) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        viewState.shopList.forEach { shop ->
-            Marker(
-                state = MarkerState(position = shop.location),
-                title = shop.name,
-                snippet = shop.access,
-                onClick = { marker ->
-                    false
-                }
+
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            viewState.shopList.forEach { shop ->
+                Marker(
+                    state = MarkerState(position = shop.location),
+                    title = shop.name,
+                    tag = shop.id,
+                    onClick = { marker ->
+                        selectedShop.value = viewState.shopList.first { it.id == marker.tag }
+                        false
+                    }
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .align(Alignment.BottomCenter)
+        ) {
+            RestaurantItemBar(
+                shop = selectedShop.value,
+                onNavDetail = onNavDetail
             )
         }
-
     }
+
 }
 
 fun zoomAll(
@@ -57,7 +85,7 @@ fun zoomAll(
     scope.launch {
         cameraPositionState.animate(
             update = CameraUpdateFactory.newLatLngBounds(boundingBox, 64),
-            durationMs = 1000
+            durationMs = 500
         )
     }
 }
