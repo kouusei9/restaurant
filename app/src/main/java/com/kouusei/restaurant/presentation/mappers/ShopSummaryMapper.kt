ec19a14c770a027067be3f19c83a73b0dc8dd52c
shanglanxin <shanglanxin@gmail.com>
2025-04-25 23:39:20 +0900

    budget logic when met empty.

--- app/src/main/java/com/kouusei/restaurant/presentation/mappers/ShopSummaryMapper.kt
+++ app/src/main/java/com/kouusei/restaurant/presentation/mappers/ShopSummaryMapper.kt
@@ -4,15 +4,13 @@ import com.google.android.gms.maps.model.LatLng
 import com.kouusei.restaurant.data.api.entities.Shop
 import com.kouusei.restaurant.presentation.entities.ShopSummary
 
-class ShopSummaryMapper {
-}
 
 fun Shop.toShopSummary(): ShopSummary {
     return ShopSummary(
         id = id,
         name = name,
         url = photo.mobile.s,
-        budget = budget.name,
+        budget = if (budget.name.isNotEmpty()) budget.name else budget.average,
         access = mobile_access,
         location = LatLng(lat, lng)
     )
